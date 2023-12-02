8011238e0ebd5a8acacc8ba0ff4b73a19478ddc9
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-12-02 10:43:31 -0800

    added separate div for formatted note result

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const note = document.getElementById("note");
+const ransom = document.getElementById("ransom");
 
+note.addEventListener("input", changeText);
+
+function changeText() {
+  // on keypress (input), change:
+  // font,
+  // font-size,
+  // text-rotation
+  // but for the **next** character inputted;
+  // i.e., keep the styling of previous characters.
+}
