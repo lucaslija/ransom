1c9668cdfc106674af0bdfa696bb1afd00f0d401
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-12-02 11:08:41 -0800

    added changeText function

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const ransom = document.getElementById("ransom");
 
 note.addEventListener("input", changeText);
 
-function changeText() {
+function changeText(e) {
   // on keypress (input), change:
   // font,
   // font-size,
   // text-rotation
   // but for the **next** character inputted;
   // i.e., keep the styling of previous characters.
+  ransom.textContent = e.target.value;
 }
