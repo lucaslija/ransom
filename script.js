a93cd27d79491484b56497cfd063c9b72ac5e99f
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-12-02 13:53:23 -0800

    new spans receive random font + font-size

--- script.js
+++ script.js
@@ -2,18 +2,53 @@ const note = document.getElementById("note");
 const ransom = document.getElementById("ransom");
 
 note.addEventListener("keydown", changeText);
+note.addEventListener("keyup", clearText);
 
-const fontArray = ['Arial', 'Verdana', 'Courier New', 'Georgia', 'Comic Sans MS'];
-const ascii = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+const fontArray = [
+  "Arial",
+  "Verdana",
+  "Courier New",
+  "Georgia",
+  "Comic Sans MS",
+];
+const ascii =
+  " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+let fontIndex = 0;
+let textSize = 1;
 
 function changeText(e) {
   if (e.key === "Backspace") {
-    // returns the string with the last span cut off
+    // removes the last span added
     ransom.removeChild(ransom.lastChild);
   } else if (ascii.includes(e.key)) {
     // don't just add the key, add it within a styled span
     const newSpan = document.createElement("span");
     newSpan.innerText = e.key;
+    if (e.key != " ") {
+      // style new single-character span with new font, new size
+      newSpan.style.fontFamily = fontArray[fontIndex];
+      newSpan.style.fontSize = `${textSize}rem`
+      // select next font from array
+      if (fontIndex == fontArray.length - 1) {
+        fontIndex = 0;
+      } else {
+        fontIndex++;
+      }
+      textSize = (Math.random() * .75 + .75);
+    }
+    // add new span
     ransom.appendChild(newSpan);
+  }
+}
+
+function clearText() {
+  if (note.value === "") {
+    removeAllChildNodes(ransom);
+  }
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
 }
