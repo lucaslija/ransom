9d71af15d1f931bd8b681cbeab6a12e440088897
Lucas Lija <lucaslija@lucas-thinkpad.ctrlh>
2023-12-15 18:15:04 -0800

    Added rotation and color changes to text

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const ransom = document.getElementById("ransom");
+const copyButton = document.getElementById("copy-button");
 
 document.addEventListener("keydown", changeText);
 document.addEventListener("keyup", clearText);
+
+copyButton.addEventListener("click", copyHTML);
 
 const fontArray = [
   "Arial",
@@ -11,11 +14,14 @@ const fontArray = [
   "Comic Sans MS",
 ];
 const weightArray = ["normal", "bold", "lighter", "bolder"];
+const colorArray = ["cornsilk", "white", "azure", "aliceblue", "antiquewhite", "beige", "bisque"]
 const ascii =
   " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 let fontIndex = 0;
 let weightIndex = 0;
+let colorIndex = 0;
 let textSize = 1;
+let textRotation = 0;
 
 function changeText(e) {
   e.preventDefault();
@@ -27,10 +33,13 @@ function changeText(e) {
     const newSpan = document.createElement("span");
     newSpan.innerText = e.key;
     if (e.key != " ") {
+      newSpan.style.display = "inline-block";
       // style new single-character span with new font, new size
       newSpan.style.fontFamily = fontArray[fontIndex];
       newSpan.style.fontWeight = weightArray[weightIndex];
+      newSpan.style.backgroundColor = colorArray[colorIndex];
       newSpan.style.fontSize = `${textSize}rem`;
+      newSpan.style.transform = `rotate(${textRotation}deg)`;
       // select next font from array
       if (fontIndex == fontArray.length - 1) {
         fontIndex = 0;
@@ -43,8 +52,16 @@ function changeText(e) {
       } else {
         weightIndex++;
       }
+      // select next color
+      if (colorIndex == colorArray.length - 1) {
+        colorIndex = 0;
+      } else {
+        colorIndex++;
+      }
       // randomize text size between 1.5 and .75 rem
       textSize = Math.random() * 0.75 + 0.75;
+      // randomize text rotation between 15 and -15 degrees
+      textRotation = Math.random() * 30 - 15;
     }
     // add new span
     ransom.appendChild(newSpan);
@@ -61,4 +78,9 @@ function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
+}
+
+function copyHTML() {
+  navigator.clipboard.writeText(ransom.innerHTML);
+  alert("Copied the raw HTML for your ransom note!");
 }
